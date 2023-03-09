a45694d2f9983ee174940e2dd897fd82ee2ed157
JaidenHuijts <jaiden02huijts@gmail.com>
2023-03-09 21:20:52 +0100

    verder aan filter

--- openfilter.js
+++ openfilter.js
@@ -1,9 +1,1 @@
-// Koppel het Handlebars-sjabloon aan de HTML
-var resultatenTemplate = Handlebars.compile(document.querySelector('#resultaten-template').innerHTML);
 
-// Verwerk het formulier bij het indienen
-document.querySelector('#filter-button').addEventListener('click', function(opendefilteraub) {
-  opendefilteraub.preventDefault();
-
-  // Haal de geselecteerde filters op
-  var sport
--- server.js
+++ server.js
@@ -13,32 +13,7 @@ app.get('/filteropen', filteropen);
 
 app.listen(1337, console.log(chalk.blue(`Running on 1337`)))
 
-const userList = [
-  {
-    id:'jespervandriel',
-    Naam: 'Jesper van Driel',
-    Leeftijd: 20,
-    Spiergroep: 'Borst',
-    Geslacht: 'Man',
-    Sportschool: 'Basic-Fit' 
-  },
-  {
-    id:'jaidenhuijts',
-    Naam: 'Jaiden Huijts',
-    Leeftijd: 21,
-    Spiergroep: 'Rug',
-    Geslacht: 'Man',
-    Sportschool: 'Big Gym' 
-  },
-  {
-    id:'jazzlinschenk',
-    Naam: 'Jazzlin Schenk',
-    Leeftijd: 19,
-    Spiergroep: 'Benen',
-    Geslacht: 'Vrouw',
-    Sportschool: 'Alle sportscholen' 
-  }
-]
+
 
 
 
@@ -50,7 +25,6 @@ app.use(express.static(__dirname + '/static'));
 app.get("/static", (req, res) => {
   res.render("static");
 });
-
 
 
 
@@ -80,11 +54,6 @@ app.set(`view engine`, `.hbs`);
 app.set(`views`, `./views`);
 
 
-
-
-
-
-
 // Filteropenpagina
 function filteropen(req, res) {
   res.render(`filteropen`, { layout: `index`})}
@@ -102,3 +71,196 @@ function filteropen(req, res) {
   
   app.set(`view engine`, `.hbs`);
   app.set(`views`, `./views`);
+
+
+
+
+
+
+
+
+
+
+
+  const userList = [
+    {
+      id:'jespervandriel',
+      Naam: 'Jesper van Driel',
+      Leeftijd: 20,
+      Spiergroep: 'Borst',
+      Geslacht: 'Man',
+      Sportschool: 'Basic-Fit',
+      Sportniveau: 'Gevorderd'
+    },
+    {
+      id:'jaidenhuijts',
+      Naam: 'Jaiden Huijts',
+      Leeftijd: 21,
+      Spiergroep: 'Rug',
+      Geslacht: 'Man',
+      Sportschool: 'Big Gym',
+      Sportniveau: 'Gevorderd'
+    },
+    {
+      id:'jazzlinschenk',
+      Naam: 'Jazzlin Schenk',
+      Leeftijd: 19,
+      Spiergroep: 'Benen',
+      Geslacht: 'Vrouw',
+      Sportschool: 'Alle sportscholen',
+      Sportniveau: 'Gemiddeld'
+    },
+    {
+      id:'joshuatourlamain',
+      Naam: 'Joshua Tourlamain',
+      Leeftijd: 20,
+      Spiergroep: 'Bicep',
+      Geslacht: 'Man',
+      Sportschool: 'Alle sportscholen',
+      Sportniveau: 'Beginner'
+    }
+  ];
+
+let geslachtButton = document.querySelector('.geslacht');
+let radioButtons = document.querySelectorAll('input[name="geslacht"]');
+
+let textoutput = document.querySelector('.textoutput');
+
+let ul = document.querySelector('ul');
+let select = document.querySelector('select');
+
+let gymgender = document.querySelector('.gymgender');
+
+select.addEventListener('change', () => {
+
+    //if de value van de dropdown is basic, trigger renderbasicfitRestult, else if value vd dropdown = big, trigger renderbigGymResult, else if value vd dropdown = free, trigger die 
+
+    if (select.value === 'Basic-Fit'){
+        renderbasicFitResult();
+    } else if (select.value === 'BigGym') {
+        renderbigGymResult();
+    } else if (select.value === 'Trainmore') {
+        rendertrainMoreResult ();
+    } else if (select.value === 'VondelGym') {
+      rendervondelGymResult ();
+    }
+});
+
+
+
+//basicFitResult is alle data waarvan de Sportschool gelijk is aan 'Basic-fit'
+const basicFitResult = userList.filter(userList => userList.Sportschool === 'Basic-Fit');
+//hetzelfde voor de andere 3
+const bigGymResult = userList.filter(userList => userList.Sportschool === 'BigGym');
+const trainMoreResult = userList.filter(userList => userList.Sportschool === 'Trainmore');
+const vondelGymResult = userList.filter(userList => userList.Sportschool === 'VondelGym');
+
+
+
+
+
+function renderbasicFitResult() {
+    //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+    clearUl();
+    basicFitResult.forEach((filterData) => {
+        allUsersHTML = `<li>
+            <p>${filterData.Naam}</p>
+        </li>`
+        ul.insertAdjacentHTML("beforeend", allUsersHTML);
+        console.log(basicFitResult);
+    });
+}
+
+function renderbigGymResult() {
+    //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+    clearUl();
+    bigGymResult.forEach((filterData) => {
+        allUsersHTML = `<li>
+            <p>${filterData.Naam}</p>
+        </li>`
+        ul.insertAdjacentHTML("beforeend", allUsersHTML);
+    });
+}
+
+function rendertrainMoreResult() {
+    //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+    clearUl();
+    trainMoreResult.forEach((filterData) => {
+        allUsersHTML = `<li>
+            <p>${filterData.Naam}</p>
+        </li>`
+        ul.insertAdjacentHTML("beforeend", allUsersHTML);
+    });
+}
+
+
+
+function rendervondelGymResult() {
+  //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+  clearUl();
+  vondelGymResult.forEach((filterData) => {
+      allUsersHTML = `<li>
+          <p>${filterData.Naam}</p>
+      </li>`
+      ul.insertAdjacentHTML("beforeend", allUsersHTML);
+  });
+}
+
+
+
+
+
+function clearUl() {
+    ul.innerHTML = "";
+}
+
+function clearP() {
+    textoutput.innerHTML = "";
+}
+
+
+
+
+function checkGeslacht() {
+    
+    let selectedGeslacht;
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedGeslacht = radioButton.value;
+            //terminate loop met break omdat je maar 1 geslacht kan zijn
+            break;
+        }
+    }
+    
+    if (selectedGeslacht === 'man' ) {
+        renderMan();
+
+    } else if(selectedGeslacht === 'vrouw') {
+        renderVrouw();
+    }
+}
+
+
+function renderMan() {
+    clearP();
+    manResult.forEach((filterData) => {
+        allUsersHTML = `
+            <p>${filterData.Naam}</p>`
+            textoutput.insertAdjacentHTML("beforeend", allUsersHTML);
+    });
+}
+
+const manResult = userList.filter(userList => userList.Geslacht === 'Man');
+
+function renderVrouw() {
+    clearP();
+    vrouwResult.forEach((filterData) => {
+        allUsersHTML = `
+            <p>${filterData.Naam}</p>`
+            textoutput.insertAdjacentHTML("beforeend", allUsersHTML);
+    });
+}
+const vrouwResult = userList.filter(userList => userList.Geslacht === 'Vrouw');
+
+
+geslachtButton.addEventListener('click', checkGeslacht);
