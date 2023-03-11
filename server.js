dddeb2cbeaa70afc6dad93d8d9a0940ff424397b
JaidenHuijts <jaiden02huijts@gmail.com>
2023-03-11 18:10:12 +0100

    database connected

--- server.js
+++ server.js
@@ -1,139 +1,115 @@
+
 const chalk = require('chalk');
-const express = require('express');
+
+
 const path = require("path");
 const { engine } = require('express-handlebars');
 const { filter } = require('lodash');
 
+// //connectie
+// app.listen(1337, console.log(chalk.blue(`Running on 1337`)))
 
-
+//database
+const express = require('express');
 const app = express();
-// require('dotenv').config({path:'.env'})
-// const PORT = process.env.PORT || 1337;
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = process.env.DB_CONNECTION_STRING;
-
-// const client = new MongoClient(
-//   uri, 
-//   {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1}
-// )
-
-// client.connect()
-// .then((res) => console.log ('@@-- connection established'))
-// .catch((err) => console.log ('@@--error', err))
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening to port: PORT`)
-// });
-
-// app.get('/', async (req, res) => {
-//   const db = client.db('GymbuddyApp').collection('Sporters');
-//   const example = await db.find({}).toArray();
+app.use(express.urlencoded({ extended: true }));
 
 
-// res.json({
-//   succes: true,
-//   message: 'connected',
-//   example
+
+ 
+require('dotenv').config({ path: '.env' });
+const PORT = process.env.PORT || 1337;
+
+
+
+//handlebars 
+app.engine(`.hbs`, engine({
+  defaultLayout: `normalstate`,
+  layoutsDir: (__dirname + `/views/layouts`),
+  partialsDir: (__dirname + `/views/partials`),
+  extname: `.hbs`
+}));
+
+app.set(`view engine`, `.hbs`);
+app.set(`views`, `./views`);
+
+app.use(express.static(__dirname + '/static'));
+app.get("/static", (req, res) => {
+  res.render("static");
+});
+app.use(express.urlencoded({ extended: true }));
+ 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_CONNECTION_STRING;
+ 
+const client = new MongoClient(
+ uri, 
+ { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+)
+ 
+client.connect()
+ .then((res) => console.log('@@-- connection established'))
+ .catch((err) => console.log('@@-- error', err))
+ 
+app.listen(PORT, () => {
+ console.log(chalk.blue(`Server listenting to port: PORT`))
+});
+ 
+app.get('/test', async (req, res) => {
+ // logic db call -> template aanroepen -> de data meegeven aan de template ||
+ const db = client.db('GymbuddyApp').collection('Sporters');
+ const example = await db.find({}).toArray();
+ console.log('@@-- data', example);
+ 
+ res.json({
+ succes: true,
+ message: 'connected',
+ example
+ })
+});
+ 
+// app.post('/filteropen', function(req, res) {
+//  // sla data op in db
 // })
 
 
 
-// app.engine(`.hbs`, engine({
-//   defaultLayout: `normalstate`,
-//   layoutsDir: (__dirname + `/views/layouts`),
-//   partialsDir: (__dirname + `/views/partials`),
-//   extname: `.hbs`
-// }));
-
-// app.set(`view engine`, `.hbs`);
-// app.set(`views`, `./views`);
-
-// app.use(express.static(__dirname + '/static'));
-// app.get("/static", (req, res) => {
-//   res.render("static");
-// });
-// app.use(express.urlencoded({ extended: true }));
 
 
 
-app.get('/', onHome);
-app.get('/filteropen', filteropen);
-// app.get('/startup', startup);
 
+//ROUTES
+// app.get('/', onHome);
+// app.get('/filteropen', filteropen);
+// // app.get('/startup', startup);
 
-// // 
-// app.post('/filteropen', (req, res) => {
-//   const userList= req.body.Sportschool;
-
-// res.send(`Voorkeuren aangepast`)
-// });
-
-app.listen(1337, console.log(chalk.blue(`Running on 1337`)))
-
-// Home pagina
-
-function onHome(req, res) {    
-  res.render(`normalstate`, { 
-    layout: `index`,
-    data: userList
-  });}
 
 app.get('/', (req, res) => {
-  onHome();
-})
+	res.render('normalstate', { layout: 'index'});
+});
 
-app.post('/filteropen', (req,res) => {
-  // data uit rquest object halen
-  const user = req.body.user
-  Collection.save(Sporters)
-})
 
-const userList = [
-  {
-    id:'jespervandriel',
-    Naam: 'Jesper van Driel',
-    Leeftijd: 20,
-    Spiergroep: 'Borst',
-    Geslacht: 'Man',
-    Sportschool: 'Basic-Fit',
-    Sportniveau: 'Gevorderd'
-  },
-  {
-    id:'jaidenhuijts',
-    Naam: 'Jaiden Huijts',
-    Leeftijd: 21,
-    Spiergroep: 'Rug',
-    Geslacht: 'Man',
-    Sportschool: 'Big Gym',
-    Sportniveau: 'Gevorderd'
-  },
-  {
-    id:'jazzlinschenk',
-    Naam: 'Jazzlin Schenk',
-    Leeftijd: 19,
-    Spiergroep: 'Benen',
-    Geslacht: 'Vrouw',
-    Sportschool: 'Alle sportscholen',
-    Sportniveau: 'Gemiddeld'
-  },
-  {
-    id:'joshuatourlamain',
-    Naam: 'Joshua Tourlamain',
-    Leeftijd: 20,
-    Spiergroep: 'Bicep',
-    Geslacht: 'Man',
-    Sportschool: 'Alle sportscholen',
-    Sportniveau: 'Beginner'
-  }
-];
 
-// Filteropenpagina
-function filteropen(req, res) {
-  res.render(`filteropen`, { data: userList, layout: `index`})}
+app.get('/filteropen', (req, res) => {
+	res.render('filteropen', { layout: 'index'});
+});
 
-  app.get('/', (req, res) => {
-    filteropen();
+
+//Error
+app.get('/*', (req, res) => {
+	res.json({
+		status: 'FAILED',
+		message: '404 - page not found'
+	});
+});
+
+
+app.get('/filteropen', async (req, res)=>{
+  await db.collection('Sporters').find({}, (error, Naam)=> {
+  if(error) console.log(error);
+  res.render('filteropen.hbs', {
+     Sporters: Naam
   })
-
+})
+})
 
--- static/js/script.js
+++ static/js/script.js
@@ -10,136 +10,136 @@ let select = document.querySelector('select');
 
 let gymgender = document.querySelector('.gymgender');
 
-select.addEventListener('change', () => {
+// select.addEventListener('change', () => {
 
-    //if de value van de dropdown is basic, trigger renderbasicfitRestult, else if value vd dropdown = big, trigger renderbigGymResult, else if value vd dropdown = free, trigger die 
+//     //if de value van de dropdown is basic, trigger renderbasicfitRestult, else if value vd dropdown = big, trigger renderbigGymResult, else if value vd dropdown = free, trigger die 
 
-    if (select.value === 'Basic-Fit'){
-        renderbasicFitResult();
-    } else if (select.value === 'BigGym') {
-        renderbigGymResult();
-    } else if (select.value === 'Trainmore') {
-        rendertrainMoreResult ();
-    } else if (select.value === 'VondelGym') {
-      rendervondelGymResult ();
-    }
-});
-
-
-
-//basicFitResult is alle data waarvan de Sportschool gelijk is aan 'Basic-fit'
-const basicFitResult = userList.filter(userList => userList.Sportschool === 'Basic-Fit');
-//hetzelfde voor de andere 3
-const bigGymResult = userList.filter(userList => userList.Sportschool === 'BigGym');
-const trainMoreResult = userList.filter(userList => userList.Sportschool === 'Trainmore');
-const vondelGymResult = userList.filter(userList => userList.Sportschool === 'VondelGym');
+//     if (select.value === 'Basic-Fit'){
+//         renderbasicFitResult();
+//     } else if (select.value === 'BigGym') {
+//         renderbigGymResult();
+//     } else if (select.value === 'Trainmore') {
+//         rendertrainMoreResult ();
+//     } else if (select.value === 'VondelGym') {
+//       rendervondelGymResult ();
+//     }
+// });
 
 
 
-
-
-function renderbasicFitResult() {
-    //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
-    clearUl();
-    basicFitResult.forEach((filterData) => {
-        allUsersHTML = `<li>
-            <p>${filterData.Naam}</p>
-        </li>`
-        ul.insertAdjacentHTML("beforeend", allUsersHTML);
-        console.log(basicFitResult);
-    });
-}
-
-function renderbigGymResult() {
-    //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
-    clearUl();
-    bigGymResult.forEach((filterData) => {
-        allUsersHTML = `<li>
-            <p>${filterData.Naam}</p>
-        </li>`
-        ul.insertAdjacentHTML("beforeend", allUsersHTML);
-    });
-}
-
-function rendertrainMoreResult() {
-    //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
-    clearUl();
-    trainMoreResult.forEach((filterData) => {
-        allUsersHTML = `<li>
-            <p>${filterData.Naam}</p>
-        </li>`
-        ul.insertAdjacentHTML("beforeend", allUsersHTML);
-    });
-}
-
-
-
-function rendervondelGymResult() {
-  //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
-  clearUl();
-  vondelGymResult.forEach((filterData) => {
-      allUsersHTML = `<li>
-          <p>${filterData.Naam}</p>
-      </li>`
-      ul.insertAdjacentHTML("beforeend", allUsersHTML);
-  });
-}
+// //basicFitResult is alle data waarvan de Sportschool gelijk is aan 'Basic-fit'
+// const basicFitResult = userList.filter(userList => userList.Sportschool === 'Basic-Fit');
+// //hetzelfde voor de andere 3
+// const bigGymResult = userList.filter(userList => userList.Sportschool === 'BigGym');
+// const trainMoreResult = userList.filter(userList => userList.Sportschool === 'Trainmore');
+// const vondelGymResult = userList.filter(userList => userList.Sportschool === 'VondelGym');
 
 
 
 
 
-function clearUl() {
-    ul.innerHTML = "";
-}
+// function renderbasicFitResult() {
+//     //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+//     clearUl();
+//     basicFitResult.forEach((filterData) => {
+//         allUsersHTML = `<li>
+//             <p>${filterData.Naam}</p>
+//         </li>`
+//         ul.insertAdjacentHTML("beforeend", allUsersHTML);
+//         console.log(basicFitResult);
+//     });
+// }
 
-function clearP() {
-    textoutput.innerHTML = "";
-}
+// function renderbigGymResult() {
+//     //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+//     clearUl();
+//     bigGymResult.forEach((filterData) => {
+//         allUsersHTML = `<li>
+//             <p>${filterData.Naam}</p>
+//         </li>`
+//         ul.insertAdjacentHTML("beforeend", allUsersHTML);
+//     });
+// }
+
+// function rendertrainMoreResult() {
+//     //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+//     clearUl();
+//     trainMoreResult.forEach((filterData) => {
+//         allUsersHTML = `<li>
+//             <p>${filterData.Naam}</p>
+//         </li>`
+//         ul.insertAdjacentHTML("beforeend", allUsersHTML);
+//     });
+// }
+
+
+
+// function rendervondelGymResult() {
+//   //Met clear haal je de hele ul leeg zodat er geen dubbele data inkomt.
+//   clearUl();
+//   vondelGymResult.forEach((filterData) => {
+//       allUsersHTML = `<li>
+//           <p>${filterData.Naam}</p>
+//       </li>`
+//       ul.insertAdjacentHTML("beforeend", allUsersHTML);
+//   });
+// }
 
 
 
 
-function checkGeslacht() {
+
+// function clearUl() {
+//     ul.innerHTML = "";
+// }
+
+// function clearP() {
+//     textoutput.innerHTML = "";
+// }
+
+
+
+
+// function checkGeslacht() {
     
-    let selectedGeslacht;
-    for (const radioButton of radioButtons) {
-        if (radioButton.checked) {
-            selectedGeslacht = radioButton.value;
-            //terminate loop met break omdat je maar 1 geslacht kan zijn
-            break;
-        }
-    }
+//     let selectedGeslacht;
+//     for (const radioButton of radioButtons) {
+//         if (radioButton.checked) {
+//             selectedGeslacht = radioButton.value;
+//             //terminate loop met break omdat je maar 1 geslacht kan zijn
+//             break;
+//         }
+//     }
     
-    if (selectedGeslacht === 'man' ) {
-        renderMan();
+//     if (selectedGeslacht === 'man' ) {
+//         renderMan();
 
-    } else if(selectedGeslacht === 'vrouw') {
-        renderVrouw();
-    }
-}
-
-
-function renderMan() {
-    clearP();
-    manResult.forEach((filterData) => {
-        allUsersHTML = `
-            <p>${filterData.Naam}</p>`
-            textoutput.insertAdjacentHTML("beforeend", allUsersHTML);
-    });
-}
-
-const manResult = userList.filter(userList => userList.Geslacht === 'Man');
-
-function renderVrouw() {
-    clearP();
-    vrouwResult.forEach((filterData) => {
-        allUsersHTML = `
-            <p>${filterData.Naam}</p>`
-            textoutput.insertAdjacentHTML("beforeend", allUsersHTML);
-    });
-}
-const vrouwResult = userList.filter(userList => userList.Geslacht === 'Vrouw');
+//     } else if(selectedGeslacht === 'vrouw') {
+//         renderVrouw();
+//     }
+// }
 
 
-geslachtButton.addEventListener('click', checkGeslacht);
+// function renderMan() {
+//     clearP();
+//     manResult.forEach((filterData) => {
+//         allUsersHTML = `
+//             <p>${filterData.Naam}</p>`
+//             textoutput.insertAdjacentHTML("beforeend", allUsersHTML);
+//     });
+// }
+
+// const manResult = userList.filter(userList => userList.Geslacht === 'Man');
+
+// function renderVrouw() {
+//     clearP();
+//     vrouwResult.forEach((filterData) => {
+//         allUsersHTML = `
+//             <p>${filterData.Naam}</p>`
+//             textoutput.insertAdjacentHTML("beforeend", allUsersHTML);
+//     });
+// }
+// const vrouwResult = userList.filter(userList => userList.Geslacht === 'Vrouw');
+
+
+// geslachtButton.addEventListener('click', checkGeslacht);
